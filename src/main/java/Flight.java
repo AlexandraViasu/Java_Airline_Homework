df94cf0d4cb328a21e3bcce69eec7b884da0338f
AlexandraViasu <viasu.alexandra@gmail.com>
2023-08-18 17:08:01 +0100

    plane done

---
+++ src/main/java/Flight.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Flight {
+    private Pilot pilot;
+    private ArrayList<CabinCrewMember> cabinCrewMemberArrayList;
+    private ArrayList<Passenger> passengers;
+    
+
+}
